81eda35a0468dfe90be2e708ce6b14e3bfb2661d
saitama0811 <goel.123.saurabh@gmail.com>
2020-04-07 16:42:23 +0530

    Initial Commit

---
+++ CatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "catalog1", "catalog2", "catalog3" };
+        }
+    }
+}
---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrdersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "order1", "order2", "order3" };
+        }
+    }
+}
